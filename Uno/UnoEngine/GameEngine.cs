5a00d3fdf96018be113f185b41f4e32a92e8acb0
iPaoloTM <paoloalipro@gmail.com>
2023-10-26 09:37:27 +0300

    fixed missing code after merge

--- Uno/UnoEngine/GameEngine.cs
+++ Uno/UnoEngine/GameEngine.cs
@@ -2,24 +2,33 @@
 
 using System.Drawing;
 using Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace UnoEngine;
 
-public  sealed class UnoEngine //i removed <TKEY> 
+public class UnoEngine //i removed <TKEY> 
 {
     
     public GameState State { get; set; } = new GameState();
-    
-    
     public List<Player> Players { get; set; } = new List<Player>();
-    public CardDeck DrawDeckOfCards { get; set; } = new CardDeck();
-    public List<Card> DiscardDeck { get; set; } = new List<Card>();
+    public CardHand CardHand { get; set; } = new CardHand();
+    public CardHand UsedHand { get; set; } = new CardHand();
+
+    public int ActivePlayerNo, CurrentRoundNo;
     
     private const int InitialHandSize = 7;
 
-    private UnoEngine (int numberOfPlayers)
+    private GameState GameState;
+
+    public UnoEngine (int numberOfPlayers)
     {
-        DrawDeckOfCards.Shuffle();
+        this.GameState = new GameState();
+        
+        CardHand.Shuffle();
         for (int i = 0; i < numberOfPlayers; i++)
         {
             Players.Add(new Player()
@@ -34,36 +43,101 @@ public  sealed class UnoEngine //i removed <TKEY>
         {
             for(int i = 0; i < numberOfPlayers; i ++)
             {
-                Players[i].Deck.Add(DrawDeckOfCards.Cards.First());
-                DrawDeckOfCards.Cards.RemoveAt(0);
+                Players[i].Hand.Add(CardHand.Cards.First());
+                CardHand.Cards.RemoveAt(0);
                 dealtCards++;
             }
         }
-        DiscardDeck.Add(DrawDeckOfCards.Cards.First());
-        DrawDeckOfCards.Cards.RemoveAt(0);
+        UsedHand.Add(CardHand.Cards.First());
+        UsedHand.Cards.RemoveAt(0);
   
-        while(DiscardDeck.First() is SpecialCard specialCard && (specialCard.Effect == EEffect.Wild || specialCard.Effect == EEffect.DrawFour))
+        while(UsedHand.First() is SpecialCard specialCard && (specialCard.Effect == EEffect.Wild || specialCard.Effect == EEffect.DrawFour))
         {
-            DiscardDeck.Insert(0, DrawDeckOfCards.Cards.First());
-            DrawDeckOfCards.Cards.RemoveAt(0);
+            UsedHand.Insert(0, CardHand.Cards.First());
+            UsedHand.Cards.RemoveAt(0);
         }
         
     }
-    
-    private static UnoEngine? _instance;
-    
-    
-    public static UnoEngine GetInstance(int numberOfPlayers = 2)
-    {
-        if (_instance == null)
-        {
-            _instance = new UnoEngine(numberOfPlayers);
-        }
-        return _instance;
-    }
-    
+
     public void PlayGame()
     {
         
     }
+
+    public void SaveGameState()
+    {
+        Console.Write("Saving Game State...");
+
+        this.GameState.GameHand = this.CardHand;
+
+        this.GameState.UsedHand = this.UsedHand;
+
+        this.GameState.Players = this.Players;
+
+        this.GameState.ActivePlayerNo = this.ActivePlayerNo;
+        
+        this.GameState.CurrentRoundNo = this.CurrentRoundNo;
+        
+        
+        Console.Write("Game State saved!");
+    }
+
+    public void ExportJSON(string filePath)
+    {
+        Console.Write("Exporting Game State to JSON...");
+
+        try
+        {
+            string GameState = "{\"CardDeck\":[";
+
+            //include GameDeck's cards
+
+            foreach (Card c in this.GameState.GameHand.Cards)
+            {
+                GameState += c.ToString() + ",";
+            }
+
+            GameState += "], \"UsedDeck\":[";
+            
+            //include UsedDeck's cards
+
+            foreach (Card c in this.GameState.UsedHand.Cards)
+            {
+                GameState += c.ToString() + ",";
+            }
+            
+            GameState += "], \"Players\":[";
+            
+            //include Player hand's cards
+            
+            foreach (Player p in this.GameState.Players)
+            {
+                GameState += p.ToString() + ",";
+            }
+
+            GameState += "],";
+            
+            //include utilities info
+
+            GameState += "\"ActivePlayerNo\":"+this.GameState.ActivePlayerNo+"\"CurrentRoundNo\":" + this.GameState.CurrentRoundNo + "";
+            
+            GameState += "}";
+            
+
+            string jsonGameState = JsonSerializer.Serialize(GameState);
+
+
+            File.WriteAllText(filePath, jsonGameState);
+
+            Console.WriteLine("Game State exported to JSON successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error exporting game state to JSON: " + ex.Message);
+        }
+    }
+
+
+
+    
 }
