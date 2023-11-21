151b241e55f1a8b79b6687023bf189f59a5f003f
HerasZ <78933654+HerasZ@users.noreply.github.com>
2023-11-21 16:36:41 +0200

    Blank new game page

---
+++ Uno/RazorWebApp/Pages/NewGame/NewGame.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWebApp.Pages.NewGame;
+
+public class NewGame : PageModel
+{
+    [BindProperty]
+    public int plyrNumber { get; set; } = default!;
+    
+    public void OnGet()
+    {
+        
+    }
+}
