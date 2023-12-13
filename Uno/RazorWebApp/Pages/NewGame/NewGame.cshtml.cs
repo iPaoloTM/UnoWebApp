b3712beb7b21c34cedae499d17b084b34ecc6172
MichalKaiser <MichalKaiser>
2023-12-13 02:55:49 +0200

    Recreating options for choosing number of players with limit from 2-10

--- Uno/RazorWebApp/Pages/NewGame/NewGame.cshtml.cs
+++ Uno/RazorWebApp/Pages/NewGame/NewGame.cshtml.cs
@@ -1,12 +1,18 @@
 ﻿using Entities.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UnoEngine;
 
 namespace RazorWebApp.Pages.NewGame;
 
 public class NewGame : PageModel
 {
+    private readonly GameEngine _gameEngine; // Assuming GameEngine is injected
 
+    public NewGame(GameEngine gameEngine)
+    {
+        _gameEngine = gameEngine;
+    }
     
     [BindProperty]
     public int PlyrNumber { get; set; } = default!;
@@ -15,4 +21,28 @@ public class NewGame : PageModel
     {
         
     }
+
+    public IActionResult OnPostStart()
+    {
+        if (!ModelState.IsValid || PlyrNumber < 2 || PlyrNumber > 10)
+        {
+            return Page();
+        }
+
+        // Process the player information
+        foreach (var player in Players)
+        {
+            if (player.IsAI)
+            {
+                // Add AI player
+                _gameEngine.AddPlayer(player.Name);
+            }
+            else
+            {
+                // Add human player
+                _gameEngine.AddPlayer(player.Name);
+            }
+        }
+    }
+
 }
