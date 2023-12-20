f51e9d7ca54f9b4462a1f9bb78292ba0af08cea9
Kateryna Biriukova <katushka@gmail.com>
2023-12-20 13:14:23 +0200

    ongoing implementation of gameflow; nothing works :( вот блин(

--- Uno/RazorWebApp/Pages/Game/Game.cshtml.cs
+++ Uno/RazorWebApp/Pages/Game/Game.cshtml.cs
@@ -10,6 +10,7 @@ public class Game : PageModel
 {
     private readonly GameRepositoryEF _gameRepository = default!;
     public GameEngine Engine;
+    public string shoutingText = default!;
     
     [BindProperty]
     public bool IsPlayerTurn { get; set; } = false;
@@ -29,5 +30,53 @@ public class Game : PageModel
         var gameState = _gameRepository.LoadGame(GameId);
         Engine.State = gameState;
         if (PlayerId == Engine.State.ActivePlayerNo) IsPlayerTurn = true;
+    }
+    
+    public void OnPostDrawCard()
+    {
+        if (Engine.TurnOver)
+        {
+            //you already acted this turn
+        }
+        else if (!Engine.CanDraw)
+        {
+            //you can play one of your cards
+        }
+        else
+        {
+            //Console.WriteLine(Engine.State.Id);
+            Console.WriteLine("EngineID: "+Engine.State.Id);
+        
+            var moveDraw = new PlayerMove(Engine.State.Players[Engine.State.ActivePlayerNo], EPlayerAction.Draw, null);
+            int success = Engine.HandlePlayerAction(moveDraw);
+            if (success != 3 && success != 1)
+            {
+                //you can play one of your cards
+            }
+        }
+    }
+
+    public void OnPostSkipTurn()
+    {
+        var gameState = _gameRepository.LoadGame(GameId);
+   
+        Engine = new GameEngine(_gameRepository)
+        {
+            State = gameState
+        };
+
+        Console.WriteLine(Engine.State.Id);
+        var moveSkip = new PlayerMove(Engine.State.Players[Engine.State.ActivePlayerNo], EPlayerAction.NextPlayer, null);
+        Engine.HandlePlayerAction(moveSkip);
+        if (!Engine.TurnOver)
+        {
+            //can't end turn without doing smth
+        }
+        _gameRepository.Save(GameId, Engine.State);
+    }
+
+    public void OnPostShout()
+    {
+        Console.WriteLine(this.shoutingText);
     }
 }
